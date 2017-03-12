c4c59ee9fb9caeeafdeb04716425a363ef12c071
--global <tr37ion@madguru.de>
2017-03-12 21:41:26 +0100

    Changes for Gitter

--- scripts/slack_complete.pl
+++ scripts/slack_complete.pl
@@ -16,48 +16,44 @@ use Irssi::Irc;
 use vars qw{$VERSION %IRSSI};
 ($VERSION) = '$Revision: 1.1 $' =~ / (\d+\.\d+) /;
 %IRSSI = (
-          name        => 'slack_complete',
-          authors     => 'Morten Lied Johansen, Jonas Berlin',
-          contact     => 'mortenjo@ifi.uio.no',
-          license     => 'GPL',
-          description => 'Prefix nicks with @ when completing nicks to match conventions on networks like Slack, Flowdock, Gitter etc',
-         );
+    name        => 'slack_complete',
+    authors     => 'Morten Lied Johansen, Jonas Berlin',
+    contact     => 'mortenjo@ifi.uio.no',
+    license     => 'GPL',
+    description => 'Prefix nicks with @ when completing nicks to match conventions on networks like Slack, Flowdock, Gitter etc',
+    );
 
 # ======[ Hooks ]=======================================================
 
 # --------[ sig_complete_slack_nick ]-----------------------------------
 
 sub sig_complete_slack_nick {
-my ($complist, $window, $word, $linestart, $want_space) = @_;
+    my ($complist, $window, $word, $linestart, $want_space) = @_;
 
-	my $wi = Irssi::active_win()->{active};
-	return unless ref $wi and $wi->{type} eq 'CHANNEL';
-	my %chatnets = map { $_ => 1 } split(/\s+/, Irssi::settings_get_str('slack_network'));
-	return unless exists $chatnets{'*'} || exists $chatnets{$wi->{server}->{chatnet}};
+    my $wi = Irssi::active_win()->{active};
+    return unless ref $wi and $wi->{type} eq 'CHANNEL';
+    my %chatnets = map { $_ => 1 } split(/\s+/, Irssi::settings_get_str('slack_network'));
+    return unless exists $chatnets{'*'} || exists $chatnets{$wi->{server}->{chatnet}};
 
-	if ($word =~ /^@/) {
-		$word =~ s/^@//;
+    if ($word =~ /^@/) {
+	$word =~ s/^@//;
+    }
+    foreach my $nick ($wi->nicks()) {
+	if ($nick->{nick} =~ /^\Q$word\E/i) {
+	    push(@$complist, "\@$nick->{nick}");
 	}
-	foreach my $nick ($wi->nicks()) {
-		if ($nick->{nick} =~ /^\Q$word\E/i) {
-		    if ($linestart) {
-    			push(@$complist, "\@$nick->{nick}");
-    	    } else {
-				push(@$complist, "\@$nick->{nick}:");
-		    }
-		}
-	}
+    }
 
-	@$complist = sort {
-		return $a =~ /^\@\Q$word\E(.*)$/i ? 0 : 1;
-	} @$complist;
+    @$complist = sort {
+	return $a =~ /^\@\Q$word\E(.*)$/i ? 0 : 1;
+    } @$complist;
 }
 
 # ======[ Setup ]=======================================================
 
 # --------[ Register settings ]-----------------------------------------
 
-Irssi::settings_add_str('slack_complete', 'slack_network', 'Slack');
+Irssi::settings_add_str('slack_complete', 'slack_network', 'gitter');
 
 # --------[ Register signals ]------------------------------------------
 
